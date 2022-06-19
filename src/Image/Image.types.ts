66ca5c8adeec9c116eb0e34dd447427910fac233
jaemil <emilhopf@gmail.com>
2022-06-19 22:46:09 +0200

    Added StaticImport type

--- src/Image/Image.types.ts
+++ src/Image/Image.types.ts
@@ -1,8 +1,20 @@
 import { HTMLAttributes, ReactNode } from 'react'
 import { UtilInterface } from '../../util/interfaces'
 
-export interface ImageProps<>extends UtilInterface {
+export interface StaticImageData {
   src: string
+  height: number
+  width: number
+  blurDataURL?: string
+}
+interface StaticRequire {
+  default: StaticImageData
+}
+
+declare type StaticImport = StaticRequire | StaticImageData
+
+export interface ImageProps<>extends UtilInterface {
+  src: string | StaticImport
   width?: string
   height?: string
   props?: any
