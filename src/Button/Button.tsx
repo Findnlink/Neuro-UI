db079ff3c97e4b5041e92edecfd7758581fffc2b
jaemil <emilhopf@gmail.com>
2022-06-23 14:02:05 +0200

    Testing goober

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -3,27 +3,33 @@ import React from 'react'
 import scss from './Button.module.scss'
 import { ButtonProps } from './Button.types'
 import { _getClassNames } from '../../util/getClassNames'
+import { styled, setup } from 'goober'
 
-import { styled } from '@stitches/react'
+//import { styled } from '@stitches/react'
+
+setup(React.createElement)
 
 const generateCSS = () => {
   const map = new Map()
 
   if (window.getComputedStyle(document.documentElement).getPropertyValue('--button-background')) {
-    map.set(
-      'backgroundColor',
-      window.getComputedStyle(document.documentElement).getPropertyValue('--button-background')
-    )
+    map.set('background', 'var(--button-background)')
   }
 
-  console.log(map)
+  if (window.getComputedStyle(document.documentElement).getPropertyValue('--button-color')) {
+    map.set('background', 'var(--button-color)')
+  }
+
+  console.log(Object.fromEntries(map))
 
   return Object.fromEntries(map)
 }
 
+const ButtonStyled = styled('button')(generateCSS())
+
 //const ButtonStyled = styled('button', generateCSS())
 
-const ButtonStyled = styled('button', generateCSS())
+//const ButtonStyled = styled('button', generateCSS())
 
 /** This is a special button */
 export const Button = ({
