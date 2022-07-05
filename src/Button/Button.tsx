00d06f375701cca47b60d76c11b0996b00e6c6d0
jaemil <emilhopf@gmail.com>
2022-07-05 21:39:18 +0200

    Publish 1.0.35

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -3,33 +3,6 @@ import React from 'react'
 import scss from './Button.module.scss'
 import { ButtonProps } from './Button.types'
 import { _getClassNames } from '../../util/getClassNames'
-import { styled, setup } from 'goober'
-
-//import { styled } from '@stitches/react'
-
-setup(React.createElement)
-
-const generateCSS = () => {
-  const map = new Map()
-
-  if (window.getComputedStyle(document.documentElement).getPropertyValue('--button-background')) {
-    map.set('background', 'var(--button-background)')
-  }
-
-  if (window.getComputedStyle(document.documentElement).getPropertyValue('--button-color')) {
-    map.set('color', 'var(--button-color)')
-  }
-
-  console.log(Object.fromEntries(map))
-
-  return Object.fromEntries(map)
-}
-
-const ButtonStyled = styled('button')(generateCSS())
-
-//const ButtonStyled = styled('button', generateCSS())
-
-//const ButtonStyled = styled('button', generateCSS())
 
 /** This is a special button */
 export const Button = ({
@@ -41,7 +14,6 @@ export const Button = ({
   htmlProps,
   style,
   variables,
-  primary,
   ...props
 }: ButtonProps) => {
   //const defaultProps = { children: 'Button', size: 'XL', ...props } as ButtonProps
@@ -59,20 +31,19 @@ export const Button = ({
   }
 
   return (
-    <ButtonStyled
+    <button
       data-testid={'button'}
       onClick={(e: any) => {
         if (disabled || loading) return
         if (onClick) onClick(e)
         if (href) window.open(href, '_blank')
       }}
-      //className={getClassNames()}
+      className={getClassNames()}
       // Warning: Received `true` for a non-boolean attribute `primary` with {...props}
-      //style={style}
-
+      style={style}
       {...htmlProps}
     >
       {children}
-    </ButtonStyled>
+    </button>
   )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.scss'
+import '../util/scss/styles.scss'
 import Docs from './Docs/Docs'
 import { BrowserRouter } from 'react-router-dom'
 
